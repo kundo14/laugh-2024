427bdab84bfde22169080e7d53edf1350802ddcd
Emiliano Yaryura <emilianoyaryurat@gmail.com>
2024-12-24 11:25:46 -0300

    fades in

--- src/components/sections/about/acting-local.tsx
+++ src/components/sections/about/acting-local.tsx
@@ -1,3 +1,4 @@
+import FadeIn from "@/components/common/animations/fade-in";
 import { CustomDraggable } from "@/components/common/draggable";
 import { cn } from "@/lib/cn";
 
@@ -9,29 +10,37 @@ export const ActingLocalSection = ({ className }: { className?: string }) => {
         className
       )}
     >
-      <img
-        src="/images/about/acting-local/3.jpg"
-        alt="Acting Local"
-        className="hidden sm:flex w-full min-h-full object-cover rounded"
-      />
+      <FadeIn delay={0.5}>
+        <img
+          src="/images/about/acting-local/3.jpg"
+          alt="Acting Local"
+          className="hidden sm:flex w-full min-h-full object-cover rounded"
+        />
+      </FadeIn>
 
       <div className="w-full flex flex-col gap-2 h-full justify-between">
-        <img
-          src="/images/about/acting-local/1.jpg"
-          alt="Acting Local"
-          className="w-full h-[calc(50%-4px)] object-cover rounded"
-        />
-        <img
-          src="/images/about/acting-local/2.jpg"
-          alt="Acting Local"
-          className="w-full h-[calc(50%-4px)] object-cover rounded"
-        />
+        <FadeIn className="h-[calc(50%-4px)]">
+          <img
+            src="/images/about/acting-local/1.jpg"
+            alt="Acting Local"
+            className="w-full h-full object-cover rounded"
+          />
+        </FadeIn>
+        <FadeIn delay={0.3} className="h-[calc(50%-4px)]">
+          <img
+            src="/images/about/acting-local/2.jpg"
+            alt="Acting Local"
+            className="w-full h-full object-cover rounded"
+          />
+        </FadeIn>
       </div>
-      <img
-        src="/images/about/acting-local/4.jpg"
-        alt="Acting Local"
-        className="hidden md:flex w-full h-full object-cover rounded"
-      />
+      <FadeIn delay={0.15}>
+        <img
+          src="/images/about/acting-local/4.jpg"
+          alt="Acting Local"
+          className="hidden md:flex w-full h-full object-cover rounded"
+        />
+      </FadeIn>
       <div className="w-full flex flex-col gap-8">
         <div className="flex flex-col -mt-12 lg:-mt-20 max-w-[220px] ml-auto">
           <p className="text-white font-playfairDisplay text-42 sm:text-48 lg:text-[56px] leading-none -ml-16">
@@ -51,11 +60,13 @@ export const ActingLocalSection = ({ className }: { className?: string }) => {
             </CustomDraggable>
           </div>
         </div>
-        <img
-          src="/images/about/acting-local/5.jpg"
-          alt="Acting Local"
-          className="w-full h-full object-cover rounded"
-        />
+        <FadeIn delay={0.4} className="h-full">
+          <img
+            src="/images/about/acting-local/5.jpg"
+            alt="Acting Local"
+            className="w-full h-full object-cover rounded"
+          />
+        </FadeIn>
       </div>
     </div>
   );
